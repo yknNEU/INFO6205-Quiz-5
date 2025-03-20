2f18da4efd73a148d7bdf73c9e404ecd24a28bc9
yknNEU <181112251+yknNEU@users.noreply.github.com>
2025-03-20 12:56:19 -0400

    Finish

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,8 +1,17 @@
 class SelectionSort {
     public static void selectionSort(int[] arr) {
         int n = arr.length;
-
-       // STUDENT TODO
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 
     public static void main(String[] args) {
